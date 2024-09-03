914fdea0c0955dddc9c3694ecaa82c23ba605569
Mohtasim <mohtasim317@hotmail.com>
2024-09-03 13:22:12 -0400

    Completed

--- transposeMatrix.js
+++ transposeMatrix.js
@@ -1,3 +1,12 @@
-    function transposeMatrix() {
+function transpose(matrix) {
+  const result = [];
 
+  for (let i = 0; i < matrix[0].length; i++) {
+    const current = Array(matrix.length).fill(0);
+    result.push(current);
+    for (let j = 0; j < matrix.length; j++) {
+      result[i][j] = matrix[j][i];
+    }
+  }
+  return result;
 }
